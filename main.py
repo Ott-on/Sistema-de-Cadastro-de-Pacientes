fdea68fa54fdfe7bb72785f9f13648576aa22999
Vitor-Moura48 <90641243+Vitor-Moura48@users.noreply.github.com>
2023-06-03 16:25:00 -0300

    Update main.py

--- main.py
+++ main.py
@@ -10,10 +10,13 @@ informacoes_registros = []
 
 informacoes_relatorios = []
 
+# Classe para cadastro de pacientes
+
 class CadastroPaciente():
   def __init__(self):
 
     pass
+
 
   #função cadastrar paciente
 
@@ -32,6 +35,7 @@ class CadastroPaciente():
     cadastro_paciente.append(input('Estado Civil do paciente: '))
 
     informacoes_pacientes.append(cadastro_paciente)
+
 
   # função remover paciente
 
@@ -60,26 +64,93 @@ class CadastroPaciente():
     
     for i in informacoes_pacientes:
       if i[1] == alterar:
-        o_que = int(input('o que você deseja alterar: nome (1), cpf (2), Email(3), Telefone(4), Celular(5), Data de nascimento(6), Sexo do paciente(7), Estado Civil(8)'))
+        o_que = int(input('o que você deseja alterar: nome (1), cpf (2), Email(3), Telefone(4), Celular(5), Data de nascimento(6), Sexo do paciente(7), Estado Civil(8): '))
         informacoes_pacientes[paciente][o_que - 1] = input('Digite o novo dado: ')
       paciente += 1
 
         
-  # função consultar informações
+  # função consultar informações de pacientes
 
   def consultar_cadastro(self):
     global informacoes_pacientes
 
-    consulta = input('Digite o cpf ou o nome do paciente que deseja consultar informações: ')
+    pedaco_nome = ''
+    nome_em_lista = []
+
+    nome_exato = False
+
+    pedaco_consulta = ''
+    consulta_em_lista = []
+
+    consulta = input('Digite o cpf, nome ou parte do nome do paciente que deseja consultar informações: ')
 
     for i in informacoes_pacientes:
+
       if i[1] == consulta or i[0] == consulta:
         print(i)
+        nome_exato = True
+        
+      
+    if nome_exato == False:
+
+
+      # separar o nome digitado em partes
+
+      for x in consulta:
+        if x != ' ':
+          pedaco_consulta += x
+                  
+        else:
+          consulta_em_lista.append(pedaco_consulta)
+          pedaco_consulta = ''
+                  
+      consulta_em_lista.append(pedaco_consulta)
+      pedaco_consulta = ''
+
+
+      # separar nome de determinado paciente em partes
+
+      for o in informacoes_pacientes:
+
+        for j in o[0]:
+
+          if j != ' ':
+            pedaco_nome += j
+
+          else:
+            nome_em_lista.append(pedaco_nome)
+            pedaco_nome = ''
+        
+        nome_em_lista.append(pedaco_nome)
+        pedaco_nome = ''
+
+
+        # conferir se alguma das partes do nome digitado é igual a alguma parte do nome do determinado paciente
+
+        igual = 0
+
+        for f in consulta_em_lista:
+          for p in nome_em_lista:
+
+            if f == p:
+
+              igual += 1
+              pass
+
+            if len(consulta_em_lista) == igual:
+              print(o)
+              break
+        nome_em_lista = []
+
     else:
-      print("\nEste paciente ainda não foi cadastrado, cadastre-o:")
+      print('/nEste paciente ainda não foi cadastrado, cadastre-o: ')
       funcionalidade_1.cadastrar_paciente()
 
+
+
     
+
+# Classe para cadastro de doenças
 
 class CadastroDoenca():
   def __init__(self):
@@ -89,16 +160,120 @@ class CadastroDoenca():
   # função  cadastrar doenças
 
   
-  
   def cadastrar_doenca(self):
     global informacoes_doencas
-    print('fewgverhjnrt6ju65')
+
     cadastro_doenca = []
 
     cadastro_doenca.append(input('Digite o código da doença: '))
     cadastro_doenca.append(input('Digite o nome da doenca: '))
 
     informacoes_doencas.append(cadastro_doenca)
+  
+  
+  # função remover cadastro de doenças
+
+  def remover_cadastro(self):
+    global informacoes_doencas
+
+    doenca = 0
+
+    remover = input('digite o código da doença que deseja remover: ')
+
+    for i in informacoes_doencas:
+      if i[0] == remover:
+        informacoes_doencas.pop(doenca)
+        
+      doenca += 1
+
+
+  # função alterar dados de uma doença
+
+  def alterar_doenca(self):
+    global informacoes_doencas
+
+    alterar = input('digite o código da doença que deseja alterar informações: ')
+    
+    doenca = 0
+    
+    for i in informacoes_doencas:
+      if i[0] == alterar:
+        o_que = int(input('o que você deseja alterar: código (1), nome (2): '))
+        informacoes_doencas[doenca][o_que - 1] = input('Digite o novo dado: ')
+    doenca += 1
+
+
+  # funcao consultar informações de doenças
+
+  def consultar_doencas(self):
+    global informacoes_doencas
+
+    pedaco_nome = ''
+    nome_em_lista = []
+
+    nome_exato = False
+
+    pedaco_consulta = ''
+    consulta_em_lista = []
+
+    consulta = input('Digite o código,nome ou parte do nome da doença que deseja consultar informações: ')
+
+    for i in informacoes_doencas:
+      if i[0] == consulta or i[1] == consulta:
+        print(i)
+        nome_exato = True
+        
+      
+    if nome_exato == False:
+
+
+      # separar o nome digitado em partes
+
+      for x in consulta:
+        if x != ' ':
+          pedaco_consulta += x
+                  
+        else:
+          consulta_em_lista.append(pedaco_consulta)
+          pedaco_consulta = ''
+                  
+      consulta_em_lista.append(pedaco_consulta)
+      pedaco_consulta = ''
+
+
+      # separar nome de determinado paciente em partes
+
+      for o in informacoes_doencas:
+
+        for j in o[1]:
+
+          if j != ' ':
+            pedaco_nome += j
+
+          else:
+            nome_em_lista.append(pedaco_nome)
+            pedaco_nome = ''
+        
+        nome_em_lista.append(pedaco_nome)
+        pedaco_nome = ''
+
+
+        # conferir se alguma das partes do nome digitado é igual a alguma parte do nome do determinado paciente
+
+        igual = 0
+
+        for f in consulta_em_lista:
+          for p in nome_em_lista:
+
+            if f == p:
+
+              igual += 1
+              pass
+
+            if len(consulta_em_lista) == igual:
+              print(o)
+              break
+        nome_em_lista = []
     
 
 
@@ -108,10 +283,11 @@ funcionalidade_1 = CadastroPaciente()
 funcionalidade_2 = CadastroDoenca()
 
 
+
 while True:
 
-  funcionalidade = input('\nCadastro do paciente (1), cadastrar código de doença (2), registrar atendimento ao paciente (3), relatório (4), encerrar (s): ')
-    
+  funcionalidade = input('\nCadastrar paciente (1), cadastrar código de doença (2), registrar atendimento ao paciente (3), relatório (4), encerrar (s): ')
+
   # menu de cadastramento do paciente
 
   if funcionalidade == '1':
@@ -127,24 +303,33 @@ while True:
     elif opcoes_cadastro_paciente == '3':
       funcionalidade_1.alterar_cadastro()
 
-    elif opcoes_cadastro_paciente == '4':
+    else:
       funcionalidade_1.consultar_cadastro()
 
-  elif funcionalidade == 's':
-    break
+
   # menu de cadastramento de doença
 
-  elif funcionalidade == '2':
+  if funcionalidade == '2':
 
     opcoes_cadastro_doenca = input('Cadastrar (1), Remover (2), Alterar (3), Consultar (4): ')
 
     if opcoes_cadastro_doenca == '1':
       funcionalidade_2.cadastrar_doenca()
-  
-  
 
-print(informacoes_pacientes)
-print(informacoes_doencas)
+    elif opcoes_cadastro_doenca == '2':
+      funcionalidade_2.remover_cadastro()
+
+    elif opcoes_cadastro_doenca == '3':
+      funcionalidade_2.alterar_doenca()
+    
+    else:
+      funcionalidade_2.consultar_doencas()
+  
+  if funcionalidade == 's':
+    break
+
+
+
 
 
 
