f74f3df183b069656a18e756ac8ae7fbc6a1838a
Tiago-msC <tiagomscunha@outlook.com>
2023-06-11 19:33:18 -0300

    andamento telas

--- main.py
+++ main.py
@@ -1,103 +1,105 @@
 from modelos.cadastro_pacientes import CadastroPacientes
 from modelos.paciente import Paciente
-from tkinter import *
-from tkinter import ttk
 
 # paciente = Paciente('Otton', '12345678910', 'otton@gmail.com', '123465678', '12345678', f'20/03/2003', 'M', 'solteiro')
 
 # cadastro = CadastroPacientes()
 # cadastro.cadastrar(paciente)
 
-import tkinter as tk 
-from tkinter import ttk 
-   
-  
-LARGEFONT =("Verdana", 35) 
-   
-class tkinterApp(tk.Tk): 
-      
-    
-    def __init__(self, *args, **kwargs):  
-        
-        tk.Tk.__init__(self, *args, **kwargs) 
-        
-        container = tk.Frame(self)   
-        container.pack(side = "top", fill = "both", expand = True)  
-        container.grid_rowconfigure(0, weight = 1) 
-        container.grid_columnconfigure(0, weight = 1) 
- 
-        self.frames = {}   
- 
-        for F in (StartPage, Page1, Page2): 
-            frame = F(container, self) 
-            self.frames[F] = frame  
-            frame.grid(row = 0, column = 0, sticky ="nsew") 
-        self.show_frame(StartPage) 
-   
-    
-    
-    def show_frame(self, cont): 
-        frame = self.frames[cont] 
-        frame.tkraise() 
-   
-   
-class StartPage(tk.Frame): 
-    def __init__(self, parent, controller):  
-        tk.Frame.__init__(self, parent) 
-        
-        label = ttk.Label(self, text ="Startpage", font = LARGEFONT) 
-        
-        label.grid(row = 0, column = 4, padx = 10, pady = 10)  
-        button1 = ttk.Button(self, text ="Page 1", 
-        command = lambda : controller.show_frame(Page1)) 
-    
-        button1.grid(row = 1, column = 1, padx = 10, pady = 10) 
- 
-        button2 = ttk.Button(self, text ="Page 2", 
-        command = lambda : controller.show_frame(Page2)) 
-    
-        button2.grid(row = 2, column = 1, padx = 10, pady = 10) 
-    
-   
-   
-class Page1(tk.Frame): 
-      
-    def __init__(self, parent, controller): 
-        tk.Frame.__init__(self, parent) 
-        label = ttk.Label(self, text ="Page 1", font = LARGEFONT) 
-        label.grid(row = 0, column = 4, padx = 10, pady = 10) 
- 
-        button1 = ttk.Button(self, text ="StartPage", 
-                            command = lambda : controller.show_frame(StartPage)) 
-    
-        button1.grid(row = 1, column = 1, padx = 10, pady = 10) 
- 
-        button2 = ttk.Button(self, text ="Page 2", 
-                            command = lambda : controller.show_frame(Page2)) 
-    
-        button2.grid(row = 2, column = 1, padx = 10, pady = 10) 
-   
-   
-   
-   
-class Page2(tk.Frame):  
-    def __init__(self, parent, controller): 
-        tk.Frame.__init__(self, parent) 
-        label = ttk.Label(self, text ="Page 2", font = LARGEFONT) 
-        label.grid(row = 0, column = 4, padx = 10, pady = 10) 
-        label2 = ttk.Label(self, text ="Page 2", font = LARGEFONT) 
-        label2.grid(row = 0, column = 8, padx = 10, pady = 10) 
- 
-        button1 = ttk.Button(self, text ="Page 1", 
-                            command = lambda : controller.show_frame(Page1)) 
-    
-        button1.grid(row = 1, column = 1, padx = 10, pady = 10) 
- 
-        button2 = ttk.Button(self, text ="Startpage", 
-                            command = lambda : controller.show_frame(StartPage)) 
-    
-        button2.grid(row = 2, column = 1, padx = 10, pady = 10) 
-   
-   
-app = tkinterApp() 
-app.mainloop() 
+import tkinter as tk
+from tkinter import PhotoImage, ttk
+
+from telas.login_medico import LoginMedico
+
+
+LARGEFONT = ("Verdana", 35)
+
+
+class tkinterApp(tk.Tk):
+
+    def __init__(self, *args, **kwargs):
+
+        tk.Tk.__init__(self, *args, **kwargs)
+        self.geometry('1000x600')  # tamanho da tela
+        self.title("Sistema de Cadastro de Pacientes")  # nome da interface
+        self.config(bg='#242323')  # cor do fundo
+        self.iconphoto(False, PhotoImage(file='imagens/icon.png'))  # icone
+        # não deixar que o usúario amplie a tela
+        self.resizable(width=False, height=False)
+
+        container = tk.Frame(self)
+        container.pack(side="top", fill="both", expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        self.frames = {}
+
+        for F in (StartPage, Page1, Page2, LoginMedico):
+            frame = F(container, self)
+            self.frames[F] = frame
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame(StartPage)
+
+    def show_frame(self, cont):
+        frame = self.frames[cont]
+        frame.tkraise()
+
+
+class StartPage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+
+        label = ttk.Label(self, text="Startpage", font=LARGEFONT)
+
+        label.grid(row=0, column=4, padx=10, pady=10)
+        button1 = ttk.Button(self, text="Login Medico",
+                             command=lambda: controller.show_frame(LoginMedico))
+
+        button1.grid(row=1, column=1, padx=10, pady=10)
+
+        button2 = ttk.Button(self, text="Page 2",
+                             command=lambda: controller.show_frame(Page2))
+
+        button2.grid(row=2, column=1, padx=10, pady=10)
+
+
+class Page1(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = ttk.Label(self, text="Page 1", font=LARGEFONT)
+        label.grid(row=0, column=4, padx=10, pady=10)
+
+        button1 = ttk.Button(self, text="StartPage",
+                             command=lambda: controller.show_frame(StartPage))
+
+        button1.grid(row=1, column=1, padx=10, pady=10)
+
+        button2 = ttk.Button(self, text="Page 2",
+                             command=lambda: controller.show_frame(Page2))
+
+        button2.grid(row=2, column=1, padx=10, pady=10)
+
+
+class Page2(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = ttk.Label(self, text="Page 2", font=LARGEFONT)
+        label.grid(row=0, column=4, padx=10, pady=10)
+        label2 = ttk.Label(self, text="Page 2", font=LARGEFONT)
+        label2.grid(row=0, column=8, padx=10, pady=10)
+
+        button1 = ttk.Button(self, text="Page 1",
+                             command=lambda: controller.show_frame(Page1))
+
+        button1.grid(row=1, column=1, padx=10, pady=10)
+
+        button2 = ttk.Button(self, text="Startpage",
+                             command=lambda: controller.show_frame(StartPage))
+
+        button2.grid(row=2, column=1, padx=10, pady=10)
+
+
+app = tkinterApp()
+app.mainloop()
--- telas/login_medico.py
+++ telas/login_medico.py
@@ -1,57 +1,78 @@
-from tkinter import *
-from tkinter import messagebox
+import tkinter as tk
+from tkinter import RAISED, RIDGE, Button, Entry, Label, PhotoImage, messagebox
 
 
-janela = Tk() #instância da janela
-janela.title("Sistema de Cadastro de Pacientes") #nome da interface
-janela.geometry('1000x600') #tamanho da tela
-janela.config(bg='#242323') #cor do fundo
-janela.iconphoto(False, PhotoImage(file='imagens/icon.png')) #icone 
-janela.resizable(width=False, height=False) #não deixar que o usúario amplie a tela
+class LoginMedico(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
 
-# entrada_usuario.delete(0,END)
-# entrada_senha.delete(0,END)
+        img = PhotoImage(file='imagens/tela_abertura.png')
+        label_image = Label(self, image=img)
+        label_image.place(x=-60, y=-5)
 
-def fazer_login():
-    usuario = entrada_usuario.get()
-    senha = entrada_senha.get()
+        label_fundobranco = Label(
+            self, width=45, height=30, background='white', relief=RIDGE)
+        label_fundobranco.place(x=638, y=50)
 
-    # Aqui verificamos se os campos foram preenchidos caso não deve mostrar um erro pedindo para preencher os campos
-    if len(usuario) != 0:
-        # Se ocorrer tudo bem iremos dazer o login
-        print(f"usuário:", usuario)
-        print(f"senha:", senha)
-    else:
-        # Aqui exibimos um popup de aviso pedindo para os campos serem preenchidos
-        messagebox.showinfo("Aviso!", "Preencha os campos para continuar", icon="warning")
+        # LOGIN:
+        label_LOGIN = Label(self, width=10, height=2, text='LOGIN', font=(
+            "Arial", 25), bg='white', fg='#02bae8')
+        label_LOGIN.place(x=700, y=70)
 
-img= PhotoImage(file='imagens/tela_abertura.png')
-label_image = Label(janela, image=img)
-label_image.place(x=-60,y=-5)
+        # Usuário:
+        label_usuario = Label(self, width=10, height=2, text='Usuário:', font=(
+            "Arial", 10), bg='white', fg='#888a89')
+        label_usuario.place(x=650, y=160)
+        entrada_usuario = Entry(self, width=14, font=(
+            "Arial", 25), bg='#636262', highlightthickness=0.5, relief='solid')
+        entrada_usuario.place(x=670, y=190)
 
-label_fundobranco = Label(janela, width=45, height=30, background='white', relief=RIDGE)
-label_fundobranco.place(x=638,y=50)
+        self.entrada_usuario = entrada_usuario.get()
 
-#LOGIN:
-label_LOGIN = Label(janela, width=10, height=2, text='LOGIN', font=("Arial", 25), bg='white',fg='#02bae8')
-label_LOGIN.place(x=700, y=70)
+        # Senha:
+        label_senha = Label(self, width=10, height=2, text='Senha:', font=(
+            "Arial", 10), bg='white', fg='#888a89')
+        label_senha.place(x=650, y=240)
+        entrada_senha = Entry(self, width=14, font=(
+            "Arial", 25), show="*", bg='#636262', highlightthickness=0.5, relief='solid')
+        entrada_senha.place(x=670, y=270)
 
-#Usuário:
-label_usuario = Label(janela, width=10, height=2, text='Usuário:', font=("Arial", 10), bg='white',fg='#888a89')
-label_usuario.place(x=650, y=160)
-entrada_usuario = Entry(janela, width=14, font=("Arial", 25), bg='#636262', highlightthickness=0.5, relief='solid')
-entrada_usuario.place(x=670, y=190)
+        self.entrada_senha = entrada_senha.get()
 
-#Senha:
-label_senha = Label(janela, width=10, height=2, text='Senha:', font=("Arial", 10), bg='white',fg='#888a89')
-label_senha.place(x=650, y=240)
-entrada_senha = Entry(janela, width=14, font=("Arial", 25), show = "*", bg='#636262', highlightthickness=0.5, relief='solid')
-entrada_senha.place(x=670, y=270)
+        # botões:
+        botao_login = Button(self, text='Entrar', width=23, command=self.fazer_login, height=2, font=(
+            "Arial", 15), bg='#02bae8', relief=RAISED, overrelief=RAISED)
+        botao_login.place(x=670, y=330)
+        botao_cadastro = Button(self, text='Cadastrar', width=23, command=self.fazer_login, height=2, font=(
+            "Arial", 15), bg='#eeeeee', relief=RAISED, overrelief=RAISED)
+        botao_cadastro.place(x=670, y=400)
 
-#botões:
-botao_login = Button(janela, text='Entrar', width=23, command=fazer_login, height=2, font=("Arial", 15), bg='#02bae8', relief=RAISED, overrelief=RAISED)
-botao_login.place(x=670, y=330)
-botao_cadastro = Button(janela, text='Cadastrar', width=23, command=fazer_login, height=2, font=("Arial", 15), bg='#eeeeee', relief=RAISED, overrelief=RAISED)
-botao_cadastro.place(x=670, y=400)
+    def fazer_login(self):
+        usuario = self.entrada_senha
+        senha = self.entrada_senha
 
-janela.mainloop()
+        print(self.entrada_senha)
+
+        # Aqui verificamos se os campos foram preenchidos caso não deve mostrar um erro pedindo para preencher os campos
+        if len(usuario) != 0:
+            # Se ocorrer tudo bem iremos dazer o login
+            print(f"usuário:", self.entrada_senha)
+            print(f"senha:", senha)
+        else:
+            # Aqui exibimos um popup de aviso pedindo para os campos serem preenchidos
+            messagebox.showinfo(
+                "Aviso!", "Preencha os campos para continuar", icon="warning")
+
+
+# janela = Tk() #instância da janela
+# janela.title("Sistema de Cadastro de Pacientes") #nome da interface
+# janela.geometry('1000x600') #tamanho da tela
+# janela.config(bg='#242323') #cor do fundo
+# janela.iconphoto(False, PhotoImage(file='imagens/icon.png')) #icone
+# janela.resizable(width=False, height=False) #não deixar que o usúario amplie a tela
+
+# # entrada_usuario.delete(0,END)
+# # entrada_senha.delete(0,END)
+
+
+# janela.mainloop()
