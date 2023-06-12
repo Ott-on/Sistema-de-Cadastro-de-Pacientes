be4639e37325be3f7b919c60a1db549e710dfd8c
Tiago-msC <tiagomscunha@outlook.com>
2023-06-12 18:46:22 -0300

    Apagando coisas que não usamos

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-from modelos.sistema_pacientes import SistemaPacientes
-from modelos.paciente import Paciente
-
 # paciente = Paciente('Otton', '12345678910', 'otton@gmail.com', '123465678', '12345678', f'20/03/2003', 'M', 'solteiro')
 
 # cadastro = CadastroPacientes()
@@ -20,7 +17,7 @@ LARGEFONT = ("Verdana", 35)
 class tkinterApp(tk.Tk):
 
     def __init__(self, *args, **kwargs):
-        
+
         tk.Tk.__init__(self, *args, **kwargs)
         self.geometry('1000x600')  # tamanho da tela
         self.title("Sistema de Cadastro de Pacientes")  # nome da interface
@@ -36,7 +33,7 @@ class tkinterApp(tk.Tk):
 
         self.frames = {}
 
-        for F in (StartPage, Page1, Page2, LoginMedico, CadastroMedico, MenuOpcoes):
+        for F in (LoginMedico, CadastroMedico, MenuOpcoes):
             frame = F(container, self)
             self.frames[F] = frame
             frame.grid(row=0, column=0, sticky="nsew")
@@ -46,65 +43,6 @@ class tkinterApp(tk.Tk):
     def show_frame(self, cont):
         frame = self.frames[cont]
         frame.tkraise()
-
-
-class StartPage(tk.Frame):
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        
-        self.configure(bg='#242323')
-
-        label = ttk.Label(self, text="Startpage", font=LARGEFONT)
-
-        label.grid(row=0, column=4, padx=10, pady=10)
-        button1 = ttk.Button(self, text="Login Medico",
-                             command=lambda: controller.show_frame(LoginMedico))
-
-        button1.grid(row=1, column=1, padx=10, pady=10)
-
-        button2 = ttk.Button(self, text="Page 2",
-                             command=lambda: controller.show_frame(CadastroMedico))
-
-        button2.grid(row=2, column=1, padx=10, pady=10)
-
-
-class Page1(tk.Frame):
-
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        self.configure(bg='#242323')
-        label = ttk.Label(self, text="Page 1", font=LARGEFONT)
-        label.grid(row=0, column=4, padx=10, pady=10)
-
-        button1 = ttk.Button(self, text="StartPage",
-                             command=lambda: controller.show_frame(StartPage))
-
-        button1.grid(row=1, column=1, padx=10, pady=10)
-
-        button2 = ttk.Button(self, text="Page 2",
-                             command=lambda: controller.show_frame(Page2))
-
-        button2.grid(row=2, column=1, padx=10, pady=10)
-
-
-class Page2(tk.Frame):
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        self.configure(bg='#242323')
-        label = ttk.Label(self, text="Page 2", font=LARGEFONT)
-        label.grid(row=0, column=4, padx=10, pady=10)
-        label2 = ttk.Label(self, text="Page 2", font=LARGEFONT)
-        label2.grid(row=0, column=8, padx=10, pady=10)
-
-        button1 = ttk.Button(self, text="Page 1",
-                             command=lambda: controller.show_frame(Page1))
-
-        button1.grid(row=1, column=1, padx=10, pady=10)
-
-        button2 = ttk.Button(self, text="Startpage",
-                             command=lambda: controller.show_frame(StartPage))
-
-        button2.grid(row=2, column=1, padx=10, pady=10)
 
 
 app = tkinterApp()
