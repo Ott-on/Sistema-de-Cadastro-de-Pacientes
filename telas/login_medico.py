08b6bd7fd3307551372d6ea39581844a9821917a
Tiago-msC <tiagomscunha@outlook.com>
2023-06-12 18:47:21 -0300

    Apagando o que não usamos e melhorando o codigo

--- telas/login_medico.py
+++ telas/login_medico.py
@@ -1,15 +1,20 @@
 import tkinter as tk
 from tkinter import RAISED, RIDGE, Button, Entry, Label, PhotoImage, messagebox
+from modelos.sistema_medico import SistemaMedico
 from telas.cadastro_medico import CadastroMedico
 from telas.menu_opcoes import MenuOpcoes
+
 
 class LoginMedico(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
+        # Instância do sistema do medico
+        sistema_medico = SistemaMedico()
+
         self.configure(bg='#242323')
         img = PhotoImage(file='imagens/tela_abertura.png')
         label_image = Label(self, image=img)
-        label_image.image = img 
+        label_image.image = img
         label_image.place(x=-60, y=-2)
 
         label_fundobranco = Label(
@@ -17,8 +22,8 @@ class LoginMedico(tk.Frame):
         label_fundobranco.place(x=638, y=50)
 
         label_escrita = Label(
-            self, width=20, height=1, text='Ainda não tem sua conta?',background='white',font=(
-            "Arial", 10))
+            self, width=20, height=1, text='Ainda não tem sua conta?', background='white', font=(
+                "Arial", 10))
         label_escrita.place(x=685, y=400)
 
         # LOGIN:
@@ -47,16 +52,15 @@ class LoginMedico(tk.Frame):
         self.entrada_senha = entrada_senha
 
         # botões:
-        botao_login = Button(self, text='Entrar', width=23, command=lambda: self.fazer_login(controller), height=2, font=(
-            "Arial", 15), bg='#02bae8',fg='white', relief=RAISED, overrelief=RAISED)
+        botao_login = Button(self, text='Entrar', width=23, command=lambda: self.fazer_login(sistema_medico, controller), height=2, font=(
+            "Arial", 15), bg='#02bae8', fg='white', relief=RAISED, overrelief=RAISED)
         botao_login.place(x=670, y=330)
-        
-        botao_cadastro = Button(self, text='Cadastra-se', width=10, command=lambda: self.fazer_cadastro(controller), height=1, font=(
-            "Arial", 10,"underline"), bg='white',fg='#02bae8', relief='solid', overrelief='solid',borderwidth=0, highlightthickness=0)
-        botao_cadastro.place(x=845, y=400)
-        
 
-    def fazer_login(self,controller):
+        botao_cadastro = Button(self, text='Cadastra-se', width=10, command=lambda: self.fazer_cadastro(sistema_medico, controller), height=1, font=(
+            "Arial", 10, "underline"), bg='white', fg='#02bae8', relief='solid', overrelief='solid', borderwidth=0, highlightthickness=0)
+        botao_cadastro.place(x=845, y=400)
+
+    def fazer_login(self, sistema_medico: SistemaMedico, controller):
         usuario = self.entrada_usuario.get()
         senha = self.entrada_senha.get()
 
@@ -65,27 +69,16 @@ class LoginMedico(tk.Frame):
             # Se ocorrer tudo bem iremos fazer o login
             print(f"usuário:", usuario)
             print(f"senha:", senha)
-            controller.show_frame(MenuOpcoes)
+            # controller.show_frame(MenuOpcoes)
+            sistema_medico.login(
+                usuario, senha, transacao_tela=lambda: controller.show_frame(MenuOpcoes))
+
         else:
             # Aqui exibimos um popup de aviso pedindo para os campos serem preenchidos
             messagebox.showinfo(
                 "Aviso!", "Preencha os campos para continuar", icon="warning")
 
-    def fazer_cadastro(self, controller):
+    def fazer_cadastro(self, sistema_medico: SistemaMedico, controller):
         # Função para realizar o cadastro
         controller.show_frame(CadastroMedico)
         print("Cadastro realizado!")
-
-
-# app = tk.Tk()
-# app.title("Sistema de Cadastro de Pacientes")
-# app.geometry('1000x600')
-# app.config(bg='#242323')
-# app.iconphoto(False, PhotoImage(file='imagens/icon.png'))
-# app.resizable(width=False, height=False)
-
-# login_medico = LoginMedico(app, app)
-# login_medico.pack()
-
-# app.mainloop()
-
