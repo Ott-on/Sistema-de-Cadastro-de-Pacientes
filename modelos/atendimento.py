e2eb669c878bb9a13ce53eb14f5334863d982a56
Tiago-msC <tiagomscunha@outlook.com>
2023-06-19 17:34:56 -0300

    adicionando a prop nome paciente na classe atendimento

--- modelos/atendimento.py
+++ modelos/atendimento.py
@@ -1,5 +1,5 @@
 class Atendimento:
-    def __init__(self, cpf_paciente, data_atendimento, hora_atendimento, peso_paciente, altura_paciente, relato_paciente, anotacoes_medico, exames_passados, diagnostico_medico, codigo_cid, tratamento_ou_medicamentos_prescritos, ):
+    def __init__(self, cpf_paciente, data_atendimento, hora_atendimento, peso_paciente, altura_paciente, relato_paciente, anotacoes_medico, exames_passados, diagnostico_medico, codigo_cid, tratamento_ou_medicamentos_prescritos, nome_paciente):
         self.cpf_paciente = cpf_paciente
         self.data_atendimento = data_atendimento
         self.hora_atendimento = hora_atendimento
@@ -11,3 +11,4 @@ class Atendimento:
         self.diagnostico_medico = diagnostico_medico
         self.codigo_cid = codigo_cid
         self.tratamento_ou_medicamentos_prescritos = tratamento_ou_medicamentos_prescritos
+        self.nome_paciente = nome_paciente
